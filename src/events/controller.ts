7b384ac1df1de32c6cb459355d85da57f6a003cf
robertlangereis <robert.langereis@gmail.com>
2019-05-06 16:31:12 +0200

    dev run start working correctly, required tables and columns are created

--- src/events/controller.ts
+++ src/events/controller.ts
@@ -2,7 +2,7 @@ import {
   JsonController, Authorized, CurrentUser, Post, Param, BadRequestError, HttpCode, NotFoundError, ForbiddenError, Get, Put, Body, Patch} from 'routing-controllers'
 import User from '../users/entity'
 import { Event, Ticket, Comment } from './entities'
-import {calculateWinner, generateRandomCard, calculatePoints} from './logic'
+// import {calculateWinner, generateRandomCard, calculatePoints} from './logic'
 import {io} from '../index'
 
 type EventList = { events: Event[] }
@@ -21,10 +21,10 @@ export default class EventController {
 
   // GET EVENT BY ID
   @Get('/events/:id')
-  getEvent(
-      @Param('id') id: number
-  ) {
-    return Event.findOneById(id)
+  async getEvent(@Param('id') id: any): Promise<Event> {
+    const event = await Event.findOne(id)
+    if (!event) throw new NotFoundError('Cannot find event')
+    return event
   }
 
   // CREATE EVENT
@@ -32,7 +32,7 @@ export default class EventController {
   @Post('/events')
   @HttpCode(201)
   async createGame(
-    @CurrentUser() user: User
+    @Body() event: Event
   ) {
     await Event.create().save()
     
@@ -43,6 +43,7 @@ export default class EventController {
 
     return event  
   }
+}
 
   // UPDATE EVENT BY ID
   // @Put('/events/:id')
@@ -51,36 +52,7 @@ export default class EventController {
   // @Body() update: Partial<EventList>
   // ) {
   // const event = await Event.findOne(id)
-  // if (!event) throw new NotFoundError('Cannot find event')
+  // if (!event) throw new BadRequestError('Event does not exist')
 
   // return Event.merge(event, update).save()
   // }
-  
-  @Authorized()
-  @Post('/events')
-  @HttpCode(201)
-  async createEvent(
-    // @CurrentUser() user: User
-    @Body() event: Event
-  ) {
-    const entity = await Event.create().save()
-    }
-  }
-
-
-    await Ticket.create({
-      event: entity, 
-      user,
-      comment
-    }).save()
-
-    const event = await Event.findOneById(entity.id)
-
-    io.emit('action', {
-      type: 'ADD_EVENT',
-      payload: event
-    })
-
-    return event
-
-}
